4345870b281e1d7747cf6ee5d6652aa70f156385
Nikolaj Dam Larsen <nikolaj@excolo.dk>
2017-04-11 20:38:58 +0200

    Creates generator project and adds initial graph structure

---
+++ TerrainQuest.Generator/Graph/INode.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace TerrainQuest.Generator.Graph
+{
+
+    /// <summary>
+    /// Represents a node in the terrain generator graph structure
+    /// </summary>
+    public interface INode
+    {
+        /// <summary>
+        /// Get a list of this nodes dependencies
+        /// </summary>
+        IEnumerable<INode> Dependencies { get; }
+
+        /// <summary>
+        /// Get whether this node is currently executing
+        /// </summary>
+        bool IsExecuting { get; }
+
+        /// <summary>
+        /// Get whether this node has finished executing
+        /// </summary>
+        bool IsDone { get; }
+
+        /// <summary>
+        /// Run any terrain processor associated with this node.
+        /// Usually, any dependencies of this node is executed and awaited first.
+        /// </summary>
+        void Execute();
+    }
+}
---
+++ TerrainQuest.Generator/Graph/ParallelNode.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TerrainQuest.Generator.Graph
+{
+
+    /// <summary>
+    /// An abstract node that executes it's dependencies in parallel 
+    /// before running it's own processor.
+    /// </summary>
+    public abstract class ParallelNode : INode
+    {
+        /// <summary>
+        /// Get whether this node is currently executing
+        /// </summary>
+        public bool IsExecuting { get; private set; }
+
+        /// <summary>
+        /// Get whether this nodes dependencies are done executing
+        /// </summary>
+        public bool IsDependenciesDone { get; private set; }
+
+        /// <summary>
+        /// Get whether this node is done executing
+        /// </summary>
+        public bool IsDone { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual IEnumerable<INode> Dependencies
+        {
+            get { yield break; }
+        }
+
+        /// <summary>
+        /// The terrain generation processor to execute once this nodes dependencies are done
+        /// </summary>
+        protected abstract void Process();
+
+        /// <summary>
+        /// Execute this node by first calling execute on all dependencies and once they finish, 
+        /// execute the Process method of this node
+        /// </summary>
+        public void Execute()
+        {
+            if (IsDone)
+                return;
+
+            IsExecuting = true;
+
+            Parallel.ForEach(Dependencies, node => node.Execute());
+            IsDependenciesDone = true;
+
+            Process();
+
+            IsExecuting = false;
+            IsDone = true;
+        }
+    }
+}
