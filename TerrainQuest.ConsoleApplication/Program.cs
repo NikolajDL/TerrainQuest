c27be930c3ad2771781a5b5656dc7898f6f67586
Nikolaj Dam Larsen <nikolaj@excolo.dk>
2017-04-13 16:31:39 +0200

    Adds a heightmap generator that generates flat maps
    - Updates console application to create a test flat heightmap using the FlatGenerator

--- TerrainQuest.ConsoleApplication/Program.cs
+++ TerrainQuest.ConsoleApplication/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TerrainQuest.Generator.Generators;
+using TerrainQuest.Generator.Graph;
 
 namespace TerrainQuest.ConsoleApplication
 {
@@ -10,6 +15,16 @@ namespace TerrainQuest.ConsoleApplication
     {
         static void Main(string[] args)
         {
+            var flatGenerator = new FlatGenerator(300, 300, 0.5d);
+            var node = new GeneratorNode(flatGenerator);
+
+            node.Execute();
+            var bitmap = node.Result.AsBitmap();
+
+            var filename = Path.Combine(Environment.CurrentDirectory, "generated.png");
+            bitmap.Save(filename, ImageFormat.Png);
+            Process.Start(filename);
+            Console.ReadKey();
         }
     }
 }
---
+++ TerrainQuest.Generator/Generators/FlatGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.Serialization;
+
+namespace TerrainQuest.Generator.Generators
+{
+    /// <summary>
+    /// A generator that generates a flat heightmap of a given height.
+    /// </summary>
+    public class FlatGenerator : IGenerator
+    {
+        private double _height;
+        private int _dimensionHeight;
+        private int _dimensionWidth;
+
+        /// <summary>
+        /// Get the height dimension of the generated <see cref="HeightMap"/>
+        /// </summary>
+        public int DimensionHeight { get { return _dimensionHeight; } }
+
+        /// <summary>
+        /// Get the width dimension of the generated <see cref="HeightMap"/>
+        /// </summary>
+        public int DimensionWidth { get { return _dimensionWidth; } }
+
+        /// <summary>
+        /// Get the height of the generated <see cref="HeightMap"/>
+        /// </summary>
+        public double Height { get { return _height; } }
+
+        /// <summary>
+        /// Create a generator, that generates a flat <see cref="HeightMap"/> of a given constant height
+        /// </summary>
+        public FlatGenerator(int height, int width, double heightValue = 0d)
+        {
+            _dimensionHeight = height;
+            _dimensionWidth = width;
+            _height = heightValue;
+        }
+
+        /// <summary>
+        /// Generate a flat heightmap.
+        /// </summary>
+        public HeightMap Generate()
+        {
+            var map = new HeightMap(DimensionHeight, DimensionWidth);
+            map.FlattenTo(Height);
+            return map;
+        }
+
+        #region Serialization
+
+        /// <summary>
+        /// Object deserialization constructor
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
+        public FlatGenerator(SerializationInfo info, StreamingContext context)
+        {
+            _dimensionHeight = info.GetInt32(nameof(DimensionHeight));
+            _dimensionWidth = info.GetInt32(nameof(DimensionWidth));
+            _height = info.GetDouble(nameof(Height));
+        }
+
+        /// <summary>
+        /// Object serialization method
+        /// </summary>
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue(nameof(DimensionHeight), DimensionHeight);
+            info.AddValue(nameof(DimensionWidth), DimensionWidth);
+            info.AddValue(nameof(Height), Height);
+        }
+
+        #endregion
+    }
+}
