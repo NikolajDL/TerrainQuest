1dff4674908fbf84292bcd6c4900f10bb4a898b4
Nikolaj Dam Larsen <nikolaj@excolo.dk>
2017-04-13 19:53:10 +0200

    Adds manual testing of node and generator serialization to console app

--- TerrainQuest.ConsoleApplication/Program.cs
+++ TerrainQuest.ConsoleApplication/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing.Imaging;
@@ -8,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TerrainQuest.Generator.Generators;
 using TerrainQuest.Generator.Graph;
+using TerrainQuest.Generator.Graph.Blending;
 
 namespace TerrainQuest.ConsoleApplication
 {
@@ -15,16 +17,60 @@ namespace TerrainQuest.ConsoleApplication
     {
         static void Main(string[] args)
         {
+            SerializeFlatGenerator();
+        }
+
+        private static void SerializeFlatGenerator()
+        {
+            var flatGenerator1 = new FlatGenerator(300, 300, 1d);
+            var flatGenerator2 = new FlatGenerator(300, 300, 0.5d);
+            var filename = GetFilename("serial.txt");
+
+            var node1 = new GeneratorNode(flatGenerator1);
+            var node2 = new GeneratorNode(flatGenerator2);
+            var mixNode = new MixNode();
+            mixNode.AddDependency(node1, 0.5f);
+            mixNode.AddDependency(node2, 0.5f);
+            
+            using (var writer = new StreamWriter(filename, false))
+            {
+                var serializer = new JsonSerializer();
+                serializer.Formatting = Formatting.Indented;
+                serializer.Serialize(writer, mixNode);
+            }
+            Process.Start(filename);
+
+            using (var reader = new StreamReader(filename))
+            {
+                var deserializer = new JsonSerializer();
+                var node = (MixNode)deserializer.Deserialize(reader, typeof(MixNode));
+
+                node.Execute();
+                var bitmap = node.Result.AsBitmap();
+
+                var filename2 = GetFilename("generated.png");
+                bitmap.Save(filename2, ImageFormat.Png);
+                Process.Start(filename2);
+            }
+
+        }
+
+        private static void GenerateFlatTestMap()
+        {
             var flatGenerator = new FlatGenerator(300, 300, 0.5d);
             var node = new GeneratorNode(flatGenerator);
 
             node.Execute();
             var bitmap = node.Result.AsBitmap();
 
-            var filename = Path.Combine(Environment.CurrentDirectory, "generated.png");
+            var filename = GetFilename("generated.png");
             bitmap.Save(filename, ImageFormat.Png);
             Process.Start(filename);
-            Console.ReadKey();
+        }
+
+        private static string GetFilename(string filename)
+        {
+            return Path.Combine(Environment.CurrentDirectory, filename);
         }
     }
 }
