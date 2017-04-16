fcb5a2cf4ddd0bf93f95b929a0bfb7082761fdf1
Nikolaj Dam Larsen <nikolaj@excolo.dk>
2017-04-16 19:37:09 +0200

    Adds HeightMap tests

---
+++ TerrainQuest.Tests/Generator/HeightMapTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerrainQuest.Generator;
+using Xunit;
+
+namespace TerrainQuest.Tests.Generator
+{
+    public class HeightMapTests
+    {
+        [Fact]
+        public void MaxHeight_ReturnsMinValueWhenInitializedWithoutSource()
+        {
+            // Arrange
+            var size = 2;
+            var map = new HeightMap(size);
+
+            // Act
+            var actual = map.MaxHeight;
+
+            // Assert
+            Assert.Equal(double.MinValue, actual);
+        }
+
+        [Fact]
+        public void MinHeight_ReturnsMaxValueWhenInitializedWithoutSource()
+        {
+            // Arrange
+            var size = 2;
+            var map = new HeightMap(size);
+
+            // Act
+            var actual = map.MinHeight;
+
+            // Assert
+            Assert.Equal(double.MaxValue, actual);
+        }
+    }
+}
