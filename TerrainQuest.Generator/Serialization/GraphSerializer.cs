f16894c43f7ed5c6918012b964e7165cce0f4104
Nikolaj Dam Larsen <nikolaj@excolo.dk>
2017-04-17 16:49:23 +0200

    Removes IFormatter serialization methods since we don't want to add the Serializable-attribute in addition to ISerializable
    - Either just serialize to JSON, or use some serialization that takes advantage of the ISerializable interface

--- TerrainQuest.Generator/Serialization/GraphSerializer.cs
+++ TerrainQuest.Generator/Serialization/GraphSerializer.cs
@@ -46,42 +46,12 @@ namespace TerrainQuest.Generator.Serialization
             }
         }
 
-
-        /// <summary>
-        /// Serialize a terrain generation <see cref="SerializedNode"/> 
-        /// using an <see cref="IFormatter"/> to the given stream.
-        /// </summary>
-        /// <param name="writer">Stream to write the serialized data to</param>
-        /// <param name="graph">Graph to serialize</param>
-        /// <param name="formatter">Formatter used to serialize the graph.</param>
-        public static void Serialize(StreamWriter writer, SerializedNode graph, IFormatter formatter)
-        {
-            formatter.Serialize(writer.BaseStream, graph);
-        }
-
-        /// <summary>
-        /// Serialize a terrain generation <see cref="SerializedNode"/> 
-        /// using an <see cref="IFormatter"/>
-        /// and saves it as the given filename. 
-        /// If the given file already exists, it'll be overridden, otherwise a new file is created.
-        /// </summary>
-        /// <param name="filename">Path and filename of the file to serialize to.</param>
-        /// <param name="graph">Graph to serialize</param>
-        /// <param name="formatter">Formatter used to serialize the graph.</param>
-        public static void Serialize(string filename, SerializedNode graph, IFormatter formatter)
-        {
-            using (var writer = new StreamWriter(filename, false))
-            {
-                Serialize(writer, graph, formatter);
-            }
-        }
-
         /// <summary>
         /// Deserializes a JSON encoded file to a terrain generation <see cref="SerializedNode"/>
         /// </summary>
         /// <param name="reader">Stream containing the JSON file to serialize</param>
         /// <returns>A terrain generation graph structure represented by a <see cref="SerializedNode"/></returns>
-        public static SerializedNode Deserialize(StreamReader reader)
+        public static SerializedNode Deserialize(TextReader reader)
         {
             var serializer = new JsonSerializer();
             return (SerializedNode)serializer.Deserialize(reader, typeof(SerializedNode));
@@ -97,34 +67,6 @@ namespace TerrainQuest.Generator.Serialization
             using (var reader = new StreamReader(filename, false))
             {
                 return Deserialize(reader);
-            }
-        }
-
-
-        /// <summary>
-        /// Deserializes a terrain generation <see cref="SerializedNode"/>
-        /// using a <see cref="IFormatter"/>.
-        /// </summary>
-        /// <param name="reader">Stream containing the JSON file to serialize</param>
-        /// <param name="formatter">Formatter used to deserialize the graph.</param>
-        /// <returns>A terrain generation graph structure represented by a <see cref="SerializedNode"/></returns>
-        public static SerializedNode Deserialize(StreamReader reader, IFormatter formatter)
-        {
-            return (SerializedNode)formatter.Deserialize(reader.BaseStream);
-        }
-
-        /// <summary>
-        /// Deserializes a terrain generation <see cref="SerializedNode"/>
-        /// using a <see cref="IFormatter"/>.
-        /// </summary>
-        /// <param name="filename">Path and name of the file to deserialize</param>
-        /// <param name="formatter">Formatter used to deserialize the graph.</param>
-        /// <returns>A terrain generation graph structure represented by a <see cref="SerializedNode"/></returns>
-        public static SerializedNode Deserialize(string filename, IFormatter formatter)
-        {
-            using (var reader = new StreamReader(filename, false))
-            {
-                return Deserialize(reader, formatter);
             }
         }
     }
