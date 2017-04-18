60e47067f73a78bbb0758980e0503bb73e29fa00
Nikolaj Dam Larsen <nikolaj@excolo.dk>
2017-04-18 22:58:32 +0200

    Adding more tests

---
+++ TerrainQuest.Tests/Generator/Graph/ImageEffectNodeTests.cs
@@ -0,0 +1,162 @@
+﻿using Moq;
+using Moq.Protected;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using TerrainQuest.Generator;
+using TerrainQuest.Generator.Effects;
+using TerrainQuest.Generator.Graph;
+using Xunit;
+
+namespace TerrainQuest.Tests.Generator.Graph
+{
+    public class ImageEffectNodeTests
+    {
+        [Fact]
+        public void Constructor_InitializesSourceAndImageEffect()
+        {
+            // Arrange
+            var source = new Mock<HeightMapNode>().Object;
+            var effect = new Mock<IEffect>().Object;
+
+            // Act
+            var actual = new ImageEffectNode(source, effect);
+
+            // Assert
+            Assert.Equal(source, actual.Source);
+            Assert.Equal(effect, actual.Effects.Single());
+        }
+
+        [Fact]
+        public void Constructor_InitializesSourceAndMultipleEffects()
+        {
+            // Arrange
+            var source = new Mock<HeightMapNode>().Object;
+            var effectA = new Mock<IEffect>().Object;
+            var effectB = new Mock<IEffect>().Object;
+
+            // Act
+            var actual = new ImageEffectNode(source, effectA, effectB);
+
+            // Assert
+            Assert.Equal(source, actual.Source);
+            Assert.Equal(effectA, actual.Effects.First());
+            Assert.Equal(effectB, actual.Effects.Skip(1).First());
+        }
+
+        [Fact]
+        public void Constructor_InitializesSourceWithNoEffects()
+        {
+            // Arrange
+            var source = new Mock<HeightMapNode>().Object;
+
+            // Act
+            var actual = new ImageEffectNode(source);
+
+            // Assert
+            Assert.Equal(source, actual.Source);
+            Assert.NotNull(actual.Effects);
+            Assert.Empty(actual.Effects);
+        }
+
+        [Fact]
+        public void Constructor_NullSourceThrowsArgumentNullException()
+        {
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => {
+                // Act
+                var actual = new ImageEffectNode(null);
+            });
+        }
+
+        [Fact]
+        public void Dependencies_ReturnsSource()
+        {
+            // Arrange
+            var source = new Mock<HeightMapNode>().Object;
+
+            // Act
+            var actual = new ImageEffectNode(source);
+
+            // Assert
+            Assert.Equal(source, actual.Dependencies.Single());
+        }
+
+        [Fact]
+        public void AddEffect_AppendsEffectToList()
+        {
+            // Arrange
+            var source = new Mock<HeightMapNode>().Object;
+            var expected = new Mock<IEffect>().Object;
+            var node = new ImageEffectNode(source);
+
+            // Act
+            node.AddEffect(expected);
+
+            // Assert
+            Assert.Equal(expected, node.Effects.Single());
+        }
+
+        [Fact]
+        public void AddEffect_NullEffectThrowsArgumentNullException()
+        {
+            // Arrange
+            var source = new Mock<HeightMapNode>().Object;
+            var node = new ImageEffectNode(source);
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => {
+                // Act
+                node.AddEffect(null);
+            });
+        }
+
+        [Fact]
+        public void ISerializable_SerializeAndDeserializeCorrectly()
+        {
+            // Arrange
+            var source = new Mock<HeightMapNode>().Object;
+            var effectA = new Mock<IEffect>().Object;
+            var effectB = new Mock<IEffect>().Object;
+            var expected = new ImageEffectNode(source, effectA, effectB);
+            var info = MockSerializationInfo<ImageEffectNode>();
+
+            // Act
+            expected.GetObjectData(info, new StreamingContext());
+            var actual = new ImageEffectNode(info, new StreamingContext());
+
+            // Assert
+            Assert.Equal(expected.Source, actual.Source);
+            Assert.Equal(expected.Effects.First(), actual.Effects.First());
+            Assert.Equal(expected.Effects.Skip(1).First(), actual.Effects.Skip(1).First());
+        }
+
+        [Fact]
+        public void Execute_RunsCalculateOnAllEffects()
+        {
+            // Arrange
+            var sourceMock = new Mock<HeightMapNode>();
+            var mockEffectA = new Mock<IEffect>();
+            mockEffectA.Setup(m => m.Calculate(It.IsAny<HeightMap>()));
+            var mockEffectB = new Mock<IEffect>();
+            mockEffectB.Setup(m => m.Calculate(It.IsAny<HeightMap>()));
+            var node = new ImageEffectNode(sourceMock.Object, mockEffectA.Object, mockEffectB.Object);
+
+            // Act
+            node.Execute();
+
+            // Assert
+            mockEffectA.Verify(m => m.Calculate(It.IsAny<HeightMap>()), Times.Once);
+            mockEffectB.Verify(m => m.Calculate(It.IsAny<HeightMap>()), Times.Once);
+        }
+
+        private static SerializationInfo MockSerializationInfo<T>()
+        {
+            var formatterConverter = new Mock<IFormatterConverter>().Object;
+            return new SerializationInfo(typeof(T), formatterConverter);
+        }
+    }
+}
---
+++ TerrainQuest.Tests/Generator/Graph/ParallelNodeTests.cs
@@ -0,0 +1,180 @@
+﻿using Moq;
+using Moq.Protected;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using TerrainQuest.Generator.Graph;
+using Xunit;
+
+namespace TerrainQuest.Tests.Generator.Graph
+{
+    public class ParallelNodeTests
+    {
+        [Fact]
+        public void Process_DependenciesAreExecutedBeforeProcess()
+        {
+            // Arrange
+            var sourceMockA = new Mock<INode>();
+            sourceMockA.Setup(m => m.Execute());
+            var sourceMockB = new Mock<INode>();
+            sourceMockB.Setup(m => m.Execute());
+
+            var node = new TestNode();
+            node.Sources.Add(sourceMockA.Object);
+            node.Sources.Add(sourceMockB.Object);
+
+            // Act
+            node.Execute();
+
+            // Assert
+            sourceMockA.Verify(m => m.Execute(), Times.Once());
+            sourceMockB.Verify(m => m.Execute(), Times.Once());
+        }
+
+        [Fact]
+        public void IsDone_ReturnsFalseByDefault()
+        {
+            // Act
+            var node = new TestNode();
+
+            // Assert
+            Assert.False(node.IsDone);
+        }
+
+        [Fact]
+        public void IsDependenciesDone_ReturnsFalseByDefault()
+        {
+            // Act
+            var node = new TestNode();
+
+            // Assert
+            Assert.False(node.IsDependenciesDone);
+        }
+
+        [Fact]
+        public void IsExecuting_ReturnsFalseByDefault()
+        {
+            // Act
+            var node = new TestNode();
+
+            // Assert
+            Assert.False(node.IsExecuting);
+        }
+
+        [Fact]
+        public void IsDone_ReturnsTrueWhenExecuteIsFinished()
+        {
+            // Arrange
+            var node = new TestNode();
+
+            // Act
+            node.Execute();
+
+            // Assert
+            Assert.True(node.IsDone);
+        }
+
+        [Fact]
+        public void IsDone_ReturnsFalseWhenProcessIsCalled()
+        {
+            // Arrange
+            var mock = new Mock<ParallelNode>();
+            mock.Protected()
+                .Setup("Process").Callback(() => {
+                    // Assert
+                    Assert.False(mock.Object.IsDone);
+                });
+
+            // Act
+            mock.Object.Execute();
+        }
+
+        [Fact]
+        public void IsDependenciesDone_ReturnsTrueWhenProcessIsCalled()
+        {
+            // Arrange
+            var mock = new Mock<ParallelNode>();
+            mock.Protected()
+                .Setup("Process").Callback(() => {
+                    // Assert
+                    Assert.True(mock.Object.IsDependenciesDone);
+                });
+
+            // Act
+            mock.Object.Execute();
+        }
+
+        [Fact]
+        public void IsExecuting_ReturnsTrueWhenDependenciesExecute()
+        {
+            // Arrange
+            var sourceMockA = new Mock<INode>();
+
+            var node = new TestNode();
+            node.Sources.Add(sourceMockA.Object);
+
+            sourceMockA.Setup(m => m.Execute()).Callback(() => {
+                // Assert
+                Assert.True(node.IsExecuting);
+            });
+
+            // Act
+            node.Execute();
+        }
+
+        [Fact]
+        public void IsExecuting_ReturnsTrueWhenProcessIsRun()
+        {
+            // Arrange
+            var mock = new Mock<ParallelNode>();
+            mock.Protected()
+                .Setup("Process").Callback(() => {
+                    // Assert
+                    Assert.True(mock.Object.IsExecuting);
+                });
+
+            // Act
+            mock.Object.Execute();
+        }
+
+        [Fact]
+        public void IsExecuting_ReturnsFalseWhenExecuteIsFinished()
+        {
+            // Arrange
+            var node = new TestNode();
+
+            // Act
+            node.Execute();
+
+            // Assert
+            Assert.False(node.IsExecuting);
+        }
+
+        [Fact]
+        public void Dependencies_IsEmptyByDefault()
+        {
+            // Arrange
+            var mock = new Mock<ParallelNode>();
+
+            // Assert
+            Assert.NotNull(mock.Object.Dependencies);
+            Assert.Empty(mock.Object.Dependencies);
+        }
+
+        private class TestNode : ParallelNode
+        {
+            public List<INode> Sources = new List<INode>();
+
+            public override IEnumerable<INode> Dependencies
+            {
+                get { return Sources; }
+            }
+
+            public override void GetObjectData(SerializationInfo info, StreamingContext context)
+            {}
+
+            protected override void Process()
+            { }
+        }
+    }
+}
