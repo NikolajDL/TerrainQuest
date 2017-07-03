f523f040e5b735f4fc56dd6172b098e188be9934
Nikolaj Dam Larsen <nikolaj@excolo.dk>
2017-07-03 23:07:16 +0200

    Adds a gaussian blur effect and associated tests

---
+++ TerrainQuest.Generator/Effects/GaussianBlurEffect.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using TerrainQuest.Generator.Helpers;
+
+namespace TerrainQuest.Generator.Effects
+{
+    /// <summary>
+    /// An effect use to blur a <see cref="HeightMap"/> using gaussian blur.
+    /// </summary>
+    public class GaussianBlurEffect : IEffect
+    {
+
+        /// <summary>
+        /// Get the deviation of the gaussian function used when <see cref="HeightMap"/> is blurred.
+        /// </summary>
+        public double Deviation { get; private set; }
+
+        /// <summary>
+        /// Get the radius of the kernel used for the gaussian blur effect. 
+        /// </summary>
+        public int? Radius { get; private set; }
+
+        /// <summary>
+        /// Create a gaussian blur image effect
+        /// </summary>
+        /// <param name="deviation">Deviation of the blur effect</param>
+        /// <param name="radius">Radius of the convolution kernel used to blur the image. 
+        /// If the radius is null, it'll be calculated from the given deviation</param>
+        public GaussianBlurEffect(double deviation, int? radius = null)
+        {
+            Deviation = deviation;
+            Radius = radius;
+        }
+
+        /// <summary>
+        /// Blur the given heightmap
+        /// </summary>
+        public HeightMap Calculate(HeightMap heightMap)
+        {
+            return CalculateTwoPassGaussianConvolution(heightMap);
+        }
+
+        private HeightMap CalculateTwoPassGaussianConvolution(HeightMap heightMap)
+        {
+            var kernel = CalculateNormalizedKernel();
+
+            var resultX = CalculateGaussianConvolution(heightMap, kernel, Direction.Horizontal);
+            var resultY = CalculateGaussianConvolution(resultX, kernel, Direction.Vertical);
+
+            return resultY;
+        }
+
+        private HeightMap CalculateGaussianConvolution(HeightMap source, double[,] kernel,
+            Direction direction)
+        {
+            var result = new HeightMap(source.Height, source.Width);
+            source.ForEach((r, c) => {
+                result[r, c] = CalculatePoint(source, r, c, kernel, direction);
+            });
+            return result;
+        }
+
+        private double[,] CalculateNormalizedKernel()
+        {
+            int radius = Radius.HasValue ? Radius.Value
+                            : (int)Math.Ceiling( Deviation * 3 ) * 2 + 1;
+            var kernel = new double[radius, 1];
+            int half = radius / 2;
+            double sum = 0d;
+            for (var i = 0; i < radius; i++)
+            {
+                kernel[i, 0] = 1 / (Math.Sqrt(2 * Math.PI) * Deviation) 
+                    * Math.Exp(-(i - half) * (i - half) / (2 * Deviation * Deviation));
+                sum += kernel[i, 0];
+            }
+            for (var i = 0; i < radius; i++)
+            {
+                kernel[i, 0] /= sum;
+            }
+
+            return kernel;
+        }
+
+        private double CalculatePoint(HeightMap source, int row, int column, 
+            double[,] kernel, Direction direction)
+        {
+            double result = 0d;
+            int half = kernel.GetLength(0) / 2;
+
+            for(var i = 0; i < kernel.GetLength(0); i++)
+            {
+                int coX = direction == Direction.Horizontal ? column + i - half : column;
+                int coY = direction == Direction.Vertical ? row + i - half : row;
+                coX = MathHelper.Clamp(coX, 0, source.Width-1);
+                coY = MathHelper.Clamp(coY, 0, source.Height-1);
+
+                result += source[coY, coX] * kernel[i, 0];
+            }
+
+            return result;
+        }
+
+        #region Serialization
+
+        /// <summary>
+        /// Object deserialization constructor
+        /// </summary>
+        public GaussianBlurEffect(SerializationInfo info, StreamingContext context)
+        {
+            Deviation = info.GetDouble(nameof(Deviation));
+            Radius = (int?)info.GetValue(nameof(Radius), typeof(int?));
+        }
+
+        /// <summary>
+        /// Object serialization method
+        /// </summary>
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue(nameof(Deviation), Deviation);
+            info.AddValue(nameof(Radius), Radius, typeof(int?));
+        }
+
+        #endregion
+
+        private enum Direction
+        {
+            Vertical,
+            Horizontal
+        }
+    }
+}
---
+++ TerrainQuest.Tests/Generator/Effects/GaussianBlurEffectTests.cs
@@ -0,0 +1,139 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using TerrainQuest.Generator;
+using TerrainQuest.Generator.Effects;
+using TerrainQuest.Generator.Helpers;
+using Xunit;
+
+namespace TerrainQuest.Tests.Generator.Effects
+{
+    public class GaussianBlurEffectTests
+    {
+        private const int Precision = 13;
+
+        [Fact]
+        public void Constructor_InitializesCorrectDeviation()
+        {
+            // Act
+            var effect = new GaussianBlurEffect(0.5d);
+
+            // Assert
+            Assert.Equal(0.5d, effect.Deviation);
+        }
+
+        [Fact]
+        public void Constructor_InitializesCorrectRadius()
+        {
+            // Act
+            var effect = new GaussianBlurEffect(0.5d, 100);
+
+            // Assert
+            Assert.Equal(100, effect.Radius);
+        }
+
+        [Fact]
+        public void Constructor_InitializesRadiusToNullByDefault()
+        {
+            // Act
+            var effect = new GaussianBlurEffect(0.5d);
+
+            // Assert
+            Assert.Null(effect.Radius);
+        }
+
+        [Fact]
+        public void ISerializable_SerializeAndDeserializeCorrectly()
+        {
+            // Arrange
+            var expected = new GaussianBlurEffect(0.5d, 100);
+            var info = MockSerializationInfo<GaussianBlurEffect>();
+
+            // Act
+            expected.GetObjectData(info, new StreamingContext());
+            var actual = new GaussianBlurEffect(info, new StreamingContext());
+
+            // Assert
+            Assert.Equal(expected.Deviation, actual.Deviation);
+            Assert.Equal(expected.Radius, actual.Radius);
+        }
+
+        [Fact]
+        public void ISerializable_SerializeAndDeserializeWithNullRadiusCorrectly()
+        {
+            // Arrange
+            var expected = new GaussianBlurEffect(0.5d);
+            var info = MockSerializationInfo<GaussianBlurEffect>();
+
+            // Act
+            expected.GetObjectData(info, new StreamingContext());
+            var actual = new GaussianBlurEffect(info, new StreamingContext());
+
+            // Assert
+            Assert.Equal(expected.Deviation, actual.Deviation);
+            Assert.Null(actual.Radius);
+        }
+
+        [Fact]
+        public void Calculate_AppliesEffectOnHeightMap()
+        {
+            // Arrange
+            var deviation = 1.5d;
+            var radius = 5;
+            var kernel = new double[5, 1] { { 0.120078384243213d }, { 0.23388075658535d }, { 0.292081718342872d }, { 0.23388075658535d }, { 0.120078384243213d } };
+            var data = new double[,] { { 0d, 0.25d, 0.5d, 0.75d, 1d } };
+            var heightMap = new HeightMap(data);
+            var expected = MockImageEffect(heightMap, kernel);
+            var effect = new GaussianBlurEffect(deviation, radius);
+
+            // Act
+            var actual = effect.Calculate(heightMap).Data;
+
+            // Assert
+            Assert.Equal(expected[0, 0], actual[0, 0], Precision);
+            Assert.Equal(expected[0, 1], actual[0, 1], Precision);
+            Assert.Equal(expected[0, 2], actual[0, 2], Precision);
+            Assert.Equal(expected[0, 3], actual[0, 3], Precision);
+            Assert.Equal(expected[0, 4], actual[0, 4], Precision);
+        }
+
+        // TODO: Test that the mean error of the blur result, 
+        // compared to "real" (convolution of gaussian function)
+        // gaussian blur, is within a reasonable threshold.
+
+        private HeightMap MockImageEffect(HeightMap heightMap, double[,] kernel)
+        {
+            var resultA = new HeightMap(heightMap.Height, heightMap.Width);
+            var resultB = new HeightMap(heightMap.Height, heightMap.Width);
+            int half = 5 / 2;
+            heightMap.ForEach((r, c) => {
+                var sum = 0d;
+                for(var i = 0; i < 5; i++)
+                {
+                    var x = MathHelper.Clamp(c + i - half, 0, 4);
+                    sum += heightMap[0, x] * kernel[i, 0];
+                }
+                resultA[r, c] = sum;
+            });
+            resultA.ForEach((r, c) => {
+                var sum = 0d;
+                for (var i = 0; i < 5; i++)
+                {
+                    sum += resultA[0, c] * kernel[i, 0];
+                }
+                resultB[r, c] = sum;
+            });
+            return resultB;
+        }
+
+        private static SerializationInfo MockSerializationInfo<T>()
+        {
+            var formatterConverter = new Mock<IFormatterConverter>().Object;
+            return new SerializationInfo(typeof(T), formatterConverter);
+        }
+    }
+}
